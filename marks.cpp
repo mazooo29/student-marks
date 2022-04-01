e3f43cf6968ffdbdc2f581c724d9b37f327d6497
Mohammad <88233962+mazooo29@users.noreply.github.com>
2022-04-01 18:05:08 +0300

    Add files via upload

---
+++ marks.cpp
@@ -0,0 +1,41 @@
+#include<iostream.h>
+int main()
+{
+	float mark;
+	cout<<"plz enter your mark"<<endl;
+	cin>>mark;
+	if(mark>=90)
+	{
+		cout<<"your grade is A"<<endl;
+	}
+	else
+	if(mark>=80)
+	{
+		cout<<"your grade is B"<<endl;
+	}
+		
+	else
+	if(mark>=70)
+	{
+		cout<<"your grade is C"<<endl;
+	}	
+		
+		else
+	if(mark>=60)
+	{
+		cout<<"your grade is D"<<endl;
+		
+	}	
+		
+		else
+	if(mark>=50)
+	{
+		cout<<"your grade is F"<<endl;
+	}
+		else
+	{
+		cout<<"SORRY MOTHER FUCKER YOU ARE FAILED |-_-|"<<endl;
+	}
+	
+	
+}
